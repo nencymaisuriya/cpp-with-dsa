38a9122a21cc8528b6b5fbd5a606475b1b87b2ad
NencyMaisuriya21 <160009308+nencymaisuriya@users.noreply.github.com>
2025-05-08 13:05:04 +0530

    Add files via upload
    cpp prg

---
+++ prg.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+main(){
+    // cout << "Name : Nency \tMaisuriya" << endl;
+    // cout << "Age  : 24" << endl;
+    // cout << "School : LPD " << endl;
+
+    // cout << " --------" << endl ;
+    // cout << "| \t |" << endl ;
+    // cout << "N \t |" << endl ;
+    // cout << "E \t |" << endl ;
+    // cout << "N \t |" << endl ;
+    // cout << "C \t |" << endl ;
+    // cout << "Y \t |" << endl ;
+    // cout << "| \t |" << endl ;
+    // cout << " --------" << endl ;
+
+    // cout  << "*" << endl << "**" << endl << "***" << endl << "**" << endl << "*" ;
+
+    // cout  <<"*"<< endl;
+    // cout  <<"*"<< endl;
+    // cout  <<"*"<< endl;
+    // cout  <<"*\t**\t*"<< endl;
+    // cout  <<"*\t*\t\t*"<< endl;
+
+    cout <<"*\n* *\n*  *\n*   *\n*    *\n*     *\n*      *\n*       *\n*\n"<<endl;
+    cout <<""<<endl;
+   
+    
+
+
+    
+}
+
