b3f54b983cb9f9570d5a9a6e393f1a3cc0f29c21
NencyMaisuriya21 <160009308+nencymaisuriya@users.noreply.github.com>
2025-05-12 11:37:20 +0530

    using variables

---
+++ project2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main(){
+    
+    int n=10,m=20;
+
+    // cout << " Addition of "<< n << " and "<< m << " is " << n+m <<endl;
+    // cout << " Subtraction of "<< n << " and "<< m << " is " << n-m <<endl;
+    // cout << " Multiplication of "<< n << " and "<< m << " is " << n*m <<endl;
+    // cout << " Division of "<< n << " and "<< m << " is " << n/m <<endl;
+    // cout << " Module of "<< n << " and "<< m << " is " << n%m <<endl;
+
+    // cout << n << " + " << m << " = " << n+m << endl;
+    // cout << n << " - " << m << " = " << n-m << endl;
+    // cout << n << " * " << m << " = " << n*m << endl;
+    // cout << n << " / " << m << " = " << n/m << endl;
+    // cout << n << " % " << m << " = " << n%m << endl;
+}
